776ed11ba989e5bba59555611300e98e87ce2f34
ALTGAME <krgpsh13@gmail.com>
2023-09-13 00:30:53 +0530

    cart

---
+++ app.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+ const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');//models
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+ app.use((req, res, next) => {
+   User.findById('')
+     .then(user => {
+       req.user = new User(user.name, user.email, user.cart, user._id);
+       next();
+     })
+     .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    'mongodb+srv://krgpsh13:4AIUcAJaBtZqSyjc@cluster0.ve7gxkg.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
